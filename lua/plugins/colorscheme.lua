8eed99628b06a0b8e8ef8d1648d653134dd54562
Gökhan Alkacir <alkacirg@gmail.com>
2025-10-26 21:47:36 +0100

    chore: update colorscheme conf

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,6 +14,7 @@ return {
       vim.g.everforest_background = "hard"
       vim.g.everforest_enable_italic = true
       vim.g.everforest_better_performance = 1
+      vim.g.everforest_transparent_background = 2
     end,
   },
 }
